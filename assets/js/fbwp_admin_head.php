50452e8e5c87bfd1a212e570cc1a5f7f527db1ae
egany <egany@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-02 07:53:17 +0000

    phong 20150502: add folders assets, includes,...
    git-svn-id: https://plugins.svn.wordpress.org/egany-facebook-to-wp/trunk@1151639 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/fbwp_admin_head.php
@@ -0,0 +1,25 @@
+<?php 
+// echo '<script>'; 
+?>
+
+<script type="text/javascript">
+
+// // var $ = jQuery;
+// var $ =jQuery.noConflict(); 
+$(document).ready(function () { // nono 
+	// $('#fb2wp_general[access_type]').change(function(){
+		// //do something... 
+		
+	// });
+});  
+
+
+
+
+//more... 
+
+</script>
+
+<?php
+// echo '</script>'; 
+?>
